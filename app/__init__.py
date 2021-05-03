9a55af6e15b4f0e7b93e52b788ab3c3938cf080a
Neil Rutherford <neilinator@hotmail.com>
2021-05-03 16:28:39 -0600

    Fixing postgres >:(

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,6 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
--- config.py
+++ config.py
@@ -9,9 +9,14 @@ load_dotenv(dotenv_path)
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    #if os.environ.get('DATABASE_URL')[0:11] == 'postgres://':
+    database_uri = os.environ.get('DATABASE_URL')
+    if database_uri[0:11] == 'postgres://':
+        SQLALCHEMY_DATABASE_URI = 'postgresql://' + database_uri.split('//')[1]
+    elif not database_uri:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    else:
+        SQLALCHEMY_DATABASE_URI = database_uri
+
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
